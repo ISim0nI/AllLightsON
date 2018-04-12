6707927180b19c5a3beef1eb1de51509932a6e16
ISim0nI <sim0nrudi@live.de>
2018-04-12 20:47:56 +0200

    StackPane

--- AllLightsON.java
+++ AllLightsON.java
@@ -3,14 +3,19 @@ package sample;
 
 import javafx.application.Application;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-
+/**
+ * TODO Make it more structured this is so bad
+ */
 public class AllLightsON extends Application {
 
 
@@ -22,6 +27,11 @@ public class AllLightsON extends Application {
 
 
         //Struktur von fenster
+
+        VBox unten = new VBox();
+        unten.setAlignment(Pos.BOTTOM_CENTER);
+        unten.getChildren().add(new Button("Reset"));
+
         TilePane root = new TilePane();
 
         root.setPrefColumns(5);
@@ -30,12 +40,18 @@ public class AllLightsON extends Application {
 
         for (int i = 0; i < 25; i++) {
             Button lampe = new Button("");
+
             root.getChildren().add(lampe);
+
 
         }
 
+        StackPane test = new StackPane();
 
-        Scene scene = new Scene(root, 400, 400);
+        test.getChildren().addAll(unten,root);
+
+
+        Scene scene = new Scene(test, 400, 400);
 
 
         primaryStage.setScene(scene);
