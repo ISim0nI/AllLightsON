240a2e9c5cb911e9b404ecbe1f73c2b9ab811016
ISim0nI <sim0nrudi@live.de>
2018-04-12 20:35:34 +0200

    imports rausgenommen

--- AllLightsON.java
+++ AllLightsON.java
@@ -1,26 +1,21 @@
 package sample;
 
-import com.sun.deploy.xml.XMLable;
+
 import javafx.application.Application;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
-import javafx.scene.Node;
+
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
+
 import javafx.scene.layout.TilePane;
-import javafx.scene.layout.VBox;
+
 import javafx.stage.Stage;
 
-import java.util.Observable;
 
 public class AllLightsON extends Application {
 
 
     @Override
     public void start(Stage primaryStage) {
-
 
 
         primaryStage.setTitle("AllLightsON");
@@ -33,9 +28,7 @@ public class AllLightsON extends Application {
         root.setPrefRows(5);
 
 
-
-
-        for (int i=0; i<25;i++){
+        for (int i = 0; i < 25; i++) {
             Button lampe = new Button("");
             root.getChildren().add(lampe);
 
